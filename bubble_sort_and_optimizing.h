d0a8b36b07c6298931aa34c8213d924b9955c703
root <root@localhost.localdomain>
2016-07-13 15:57:08 +0800

    add notes

--- bubble_sort_and_optimizing.h
+++ bubble_sort_and_optimizing.h
@@ -61,8 +61,9 @@ void improved_bubble_sort_v1(T *arr, int length)
 }
 
 /*
- *
- *
+ * 做进一步优化。如果有100个数的数组，仅前面10个无序，后面90个都已排好序且都大于前面10个数字，
+ * 那么在第一趟遍历后，最后发生交换的位置必定小于10，且这个位置之后的数据必定已有序了，
+ * 记录下这个位置，第二次只要从数组头部遍历到这个位置就可以了。
  */
 template <typename T>
 void improved_bubble_sort_v2(T *arr, int length)
